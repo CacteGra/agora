fcede293df6c208698cad7b896b247a73be856f2
CacteGra_local <tetoro@Tetoros-Macbook-Air.local>
2019-02-06 19:48:18 +0100

    absolute path error

--- agora/settings.py
+++ agora/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 USE_X_FORWARDED_HOST = True
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(os.path.dirname(PROJECT_DIR), 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
